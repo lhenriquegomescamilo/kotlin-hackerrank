b9e2423b99adcaca30a2640ae42736e6a520b9b3
lcamilo <lhenriquegomescamilo@gmail.com>
2022-01-08 14:00:50 +0000

    Added challeng of two sum

---
+++ src/main/kotlin/com/camilo/TwoSum.kt
@@ -0,0 +1,18 @@
+package com.camilo
+
+class TwoSum {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        var firstIndex = 0
+        var secondIndex = 0
+        for (i in nums.indices) {
+            for (j in (i + 1) until nums.size) {
+                val result = nums[i] + nums[j]
+                if (result == target) {
+                    firstIndex = i
+                    secondIndex = j
+                }
+            }
+        }
+        return intArrayOf(firstIndex, secondIndex)
+    }
+}
---
+++ src/test/kotlin/com/camilo/TwoSumTest.kt
@@ -0,0 +1,20 @@
+package com.camilo
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class TwoSumTest {
+
+    @Test
+    fun `it should return 1,1 to input 3,3 and target is 6`() {
+        val nums = intArrayOf(3, 3)
+        val twoSum = TwoSum().twoSum(nums, 6)
+        assertArrayEquals(intArrayOf(0, 1), twoSum)
+    }
+    @Test
+    fun `it should return 0,1 to input 2,7,11,15 and target is 9`() {
+        val nums = intArrayOf(2,7,11,15)
+        val twoSum = TwoSum().twoSum(nums, 9)
+        assertArrayEquals(intArrayOf(0, 1), twoSum)
+    }
+}
