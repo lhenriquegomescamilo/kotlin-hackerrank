cdf43ed27035a05660a66c925345c3dc8dc1731d
lcamilo <lhenriquegomescamilo@gmail.com>
2022-01-09 19:29:51 +0000

    Using the memoization to store the repated values.

--- src/main/kotlin/com/camilo/lessons/dynamic/Fibonacci.kt
+++ src/main/kotlin/com/camilo/lessons/dynamic/Fibonacci.kt
@@ -1,7 +1,45 @@
 package com.camilo.lessons.dynamic
 
 class Fibonacci {
+    /**
+     *                              fib(5)
+     *                     fib(4)             fib(3)
+     *               fib(3)     fib(2)  fib(2)    fib(1)
+     *            fib(2)  fib(1)
+     *
+     * The time complexity is O(2^n) and some operations like fib(2) are repeated 3 times and fib(3) are repeated 2 time,
+     *  they are lost computate
+     */
     fun fibbonacciRecurson(n: Long): Long =
         if (n == 1L || n == 2L) 1L
         else fibbonacciRecurson(n - 1) + fibbonacciRecurson(n - 2)
+
+
+    /**
+     *                              fib(5)
+     *                     fib(4)             fib(3)
+     *               fib(3)     fib(2)  fib(2)    fib(1)
+     *            fib(2)  fib(1)
+     *
+     * The time complexity is T(n) = #callFunction * t
+     *     #callFunction = Number of call of function
+     *     # t = time the take the value of value from array
+     * The example:
+     *      T(n) = O(2n + 1) * O(1)
+     *      T(n) = O(2n + 1)
+     *      T(n) = O(2n)
+     */
+    fun fibbonacciMemoize(n: Long, memo: MutableMap<Long, Long> = mutableMapOf()): Long {
+        var result: Long = 0
+        if (memo.contains(n)) {
+            return memo[n]!!
+        } else if (n == 1L || n == 2L) {
+            result = 1L
+        } else {
+            result = fibbonacciMemoize(n - 1, memo) + fibbonacciMemoize(n - 2, memo)
+        }
+
+        memo[n] = result
+        return result
+    }
 }
--- src/test/kotlin/com/camilo/lessons/dynamic/FibonacciTest.kt
+++ src/test/kotlin/com/camilo/lessons/dynamic/FibonacciTest.kt
@@ -6,9 +6,16 @@ import org.junit.jupiter.api.Test
 class FibonacciTest {
 
     @Test
-    fun `it should return 5 to fibonaaci(5)`() {
+    fun `it should return 5 to fibonaaci(5) using the recursons`() {
         val input: Long = 5
         val result = Fibonacci().fibbonacciRecurson(input)
+        assertEquals(input, result)
+    }
+
+    @Test
+    fun `it should return 5 to fibonaaci(5) using the memoized solutions`() {
+        val input: Long = 5
+        val result = Fibonacci().fibbonacciMemoize(input)
         assertEquals(input, result)
     }
 }
