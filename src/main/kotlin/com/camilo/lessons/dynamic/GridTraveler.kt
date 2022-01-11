81bdf498b16db73571d29f6af38aff9140962076
lcamilo <lhenriquegomescamilo@gmail.com>
2022-01-11 09:12:30 +0000

    Calculating the number of possible path that the traveller could be.

--- src/main/kotlin/com/camilo/lessons/dynamic/GridTraveler.kt
+++ src/main/kotlin/com/camilo/lessons/dynamic/GridTraveler.kt
@@ -1,9 +1,12 @@
 package com.camilo.lessons.dynamic
 
 class GridTraveler {
-    fun gridTraveler(n: Long, m: Long, incrementer: Long = 0): Long {
-        if (n <= 1L && m <= 1) return incrementer
-        return if (m == 1L) gridTraveler(n - 1L, m, incrementer + 1L)
-        else gridTraveler(n, m - 1, incrementer + 1L)
+    fun gridTraveler(n: Long, m: Long, memo: MutableMap<Pair<Long, Long>, Long> = mutableMapOf()): Long {
+        if (n <= 1L && m <= 1) return 1
+        if (n == 0L || m == 0L) return 0
+        if (memo.containsKey(Pair(n, m))) return memo[Pair(n, m)]!!
+        val result = gridTraveler(n - 1, m, memo) + gridTraveler(n, m - 1, memo)
+        memo[Pair(n, m)] = result
+        return result
     }
 }
--- src/test/kotlin/com/camilo/lessons/dynamic/GridTravelerTest.kt
+++ src/test/kotlin/com/camilo/lessons/dynamic/GridTravelerTest.kt
@@ -1,13 +1,34 @@
 package com.camilo.lessons.dynamic
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 
+@TestMethodOrder(MethodOrderer.DisplayName::class)
 internal class GridTravelerTest {
 
     @Test
-    fun `the travel shold walk 4 step to grid 3,3 `() {
+    fun `the travel should walk 6 step to grid 3,3 `() {
         val result = GridTraveler().gridTraveler(3, 3)
-        assertEquals(4, result)
+        assertEquals(6, result)
+    }
+
+    @Test
+    fun `the travel should walk 3 step to grid 2,3 `() {
+        val result = GridTraveler().gridTraveler(2, 3)
+        assertEquals(3, result)
+    }
+
+    @Test
+    fun `the travel should walk 3 step to grid 3,2 `() {
+        val result = GridTraveler().gridTraveler(3, 2)
+        assertEquals(3, result)
+    }
+
+    @Test
+    fun `the travel should walk 2333606220 step to grid 18,18 `() {
+        val result = GridTraveler().gridTraveler(18, 18)
+        assertEquals(2333606220, result)
     }
 }
