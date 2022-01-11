d8123b49981f60772c8de8956965aabeca69e230
lcamilo <lhenriquegomescamilo@gmail.com>
2022-01-11 23:09:28 +0000

    Sum roo to leaft binary.

--- src/main/kotlin/com/camilo/leetcode/SumRootToLeaf.kt
+++ src/main/kotlin/com/camilo/leetcode/SumRootToLeaf.kt
@@ -4,17 +4,32 @@ package com.camilo.leetcode
  * The problem is: https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/
  */
 class SumRootToLeaf {
-    fun run(root: TreeNode?): Int {
-        var acc = ""
-        var currentNode: TreeNode? = root
-        while (true) {
-            acc += currentNode?.value
-            acc += ';'
+    fun run(root: TreeNode?): Int = collect(root)
 
-            if (currentNode?.left == null && currentNode?.right == null) break
-            currentNode = currentNode?.left
+    private fun collect(root: TreeNode?): Int {
+        var rootToLeaf = 0
+        var currentNumber = 0
+        var rootNode = root
+        val stack = ArrayDeque<Pair<TreeNode?, Int>>()
+        stack.addFirst(Pair(root, 0))
+        while (stack.isNotEmpty()) {
+            val (key, value) = stack.removeLast()
+            rootNode = key
+            currentNumber = value
+
+            if (rootNode != null) {
+                currentNumber = (currentNumber shl 1) or rootNode!!.value
+
+                if (rootNode.left == null && rootNode.right == null) {
+                    rootToLeaf += currentNumber
+                } else {
+                    stack.addFirst(Pair(rootNode.right, currentNumber))
+                    stack.addFirst(Pair(rootNode.left, currentNumber))
+                }
+            }
         }
-        return 0
+
+        return rootToLeaf
     }
 }
 
