048c154e974f70ac5dce2b5e094effe9bd998fb1
lcamilo <lhenriquegomescamilo@gmail.com>
2022-01-12 23:56:23 +0000

    Added the video that explain how works this algorithm

--- src/main/kotlin/com/camilo/leetcode/MaximumSubArray.kt
+++ src/main/kotlin/com/camilo/leetcode/MaximumSubArray.kt
@@ -5,11 +5,11 @@ class MaximumSubArray {
      * Example: [-2,1,-3,4,-1,2,1,-5,4]
      * output is: 6
      * because the continous subarray is= 4,-1,2,1 = 6
+     * description in this video: https://www.youtube.com/watch?v=2MmGzdiKR9Y
      */
     fun maxSubArray(nums: IntArray): Int {
         var max = Int.MIN_VALUE
         var sum = 0
-
         for (i in nums.indices) {
             sum += nums[i]
             max = sum.coerceAtLeast(max)
