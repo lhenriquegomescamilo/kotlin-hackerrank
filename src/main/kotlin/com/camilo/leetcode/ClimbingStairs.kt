3425500eb715fc7401661ce287c6bdca6b5f335a
lcamilo <lhenriquegomescamilo@gmail.com>
2022-01-13 23:12:06 +0000

    Exploring the others solutions.

--- src/main/kotlin/com/camilo/leetcode/ClimbingStairs.kt
+++ src/main/kotlin/com/camilo/leetcode/ClimbingStairs.kt
@@ -3,16 +3,32 @@ package com.camilo.leetcode
 class ClimbingStairs {
     /**
      * https://www.youtube.com/watch?v=NFJ3m9a1oJQ
+     * https://leetcode.com/problems/climbing-stairs/
      */
-    fun solve(n: Int): Int {
+    fun solutionBottonUp(n: Int): Int {
         var one = 1
         var two = 1
-        for( i in 0 until (n - 1)){
+        for (i in 0 until (n - 1)) {
             val temp = one
             one += two
             two = temp
         }
         return one
+    }
+
+    fun recursiveSolution(n: Int): Int {
+        if (n < 0) return 0
+        if (n == 1 || n == 0) return 1
+        return recursiveSolution(n - 1) + recursiveSolution(n - 2)
+    }
+
+    fun memoizeSolution(n: Int, memo: MutableMap<Int, Int> = mutableMapOf()): Int {
+        if (memo.containsKey(n)) return memo[n]!!
+        if (n < 0) return 0
+        if (n == 1 || n == 0) return 1
+        val result = memoizeSolution(n - 1) + memoizeSolution(n - 2)
+        memo[n] = result
+        return result
     }
 
 }
--- src/test/kotlin/com/camilo/leetcode/ClimbingStairsTest.kt
+++ src/test/kotlin/com/camilo/leetcode/ClimbingStairsTest.kt
@@ -6,16 +6,44 @@ import org.junit.jupiter.api.Test
 internal class ClimbingStairsTest {
 
     @Test
-    fun `it should return 3 step to input 3`() {
+    fun `it should return 3 step to input 3 bottom up`() {
         val input = 3
-        val result = ClimbingStairs().solve(input)
+        val result = ClimbingStairs().solutionBottonUp(input)
         assertEquals(3, result)
     }
 
     @Test
-    fun `it should return 5 step to input 4`() {
+    fun `it should return 5 step to input 4 bottom up`() {
         val input = 4
-        val result = ClimbingStairs().solve(input)
+        val result = ClimbingStairs().solutionBottonUp(input)
+        assertEquals(5, result)
+    }
+
+    @Test
+    fun `it should return 3 step to input 3 recursive solution`() {
+        val input = 3
+        val result = ClimbingStairs().recursiveSolution(input)
+        assertEquals(3, result)
+    }
+
+    @Test
+    fun `it should return 5 step to input 4 recursive`() {
+        val input = 4
+        val result = ClimbingStairs().recursiveSolution(input)
+        assertEquals(5, result)
+    }
+
+    @Test
+    fun `it should return 3 step to input 3 memoize solution`() {
+        val input = 3
+        val result = ClimbingStairs().memoizeSolution(input)
+        assertEquals(3, result)
+    }
+
+    @Test
+    fun `it should return 5 step to input 4 memoizeSolution`() {
+        val input = 4
+        val result = ClimbingStairs().memoizeSolution(input)
         assertEquals(5, result)
     }
 }
