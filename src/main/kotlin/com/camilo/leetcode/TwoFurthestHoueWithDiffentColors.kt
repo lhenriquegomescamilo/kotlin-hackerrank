a6c59b104bbba3082bacea6d6a2eed5cf74cd638
lcamilo <lhenriquegomescamilo@gmail.com>
2022-01-15 21:32:26 +0000

    Calculate the max distance in the between diferent houses with link of the challenge.

--- src/main/kotlin/com/camilo/leetcode/TwoFurthestHoueWithDiffentColors.kt
+++ src/main/kotlin/com/camilo/leetcode/TwoFurthestHoueWithDiffentColors.kt
@@ -1,27 +1,27 @@
 package com.camilo.leetcode
 
-import kotlin.math.abs
-
 
 class TwoFurthestHoueWithDiffentColors {
     /**
+     * The challenge: https://leetcode.com/problems/two-furthest-houses-with-different-colors
      * The time complexity is O(n)
+     *
      */
     fun maxDistance(colors: IntArray): Int {
-        var max = 0
+        var max = 0;
         val right = 0
         val rightColor = colors[right]
         val left = colors.size - 1
         val leftColor = colors[left]
         for (i in colors.indices) {
-            val currentValue = colors[i]
-
-            if (currentValue != rightColor) {
-                max = Math.max(max, abs(right - i))
+            val currenColor = colors[i]
+            if (currenColor != rightColor) {
+                val rightAbs = Math.abs(right - i)
+                max = Math.max(max, rightAbs)
             }
-
-            if (currentValue != leftColor) {
-                max = Math.max(max, abs(left - i))
+            if (currenColor != leftColor) {
+                val leftAbs = Math.abs(left - i)
+                max = Math.max(max, leftAbs)
             }
         }
         return max
