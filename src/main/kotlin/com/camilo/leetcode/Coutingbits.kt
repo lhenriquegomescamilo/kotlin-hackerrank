5d3ea1973fa7dc2c46c6e4234bf8d3c3756c259d
lcamilo <lhenriquegomescamilo@gmail.com>
2022-01-17 22:39:45 +0000

    Using the dynamic programming to count the number of bits.

--- src/main/kotlin/com/camilo/leetcode/Coutingbits.kt
+++ src/main/kotlin/com/camilo/leetcode/Coutingbits.kt
@@ -9,7 +9,20 @@ class Coutingbits {
         // Time complexity if O(n^2) because the count it's an inside loop
         for(i in 0 until n + 1) {
             val currentNumber = i.toString(radix = 2).count { it == '1' }
-            output[i] = currentNumber.toInt()
+            output[i] = currentNumber
+        }
+        return output
+    }
+
+    fun countBitsMoreDynamicProgramming(n: Int): IntArray {
+        // Space completixy is O(n)
+        val output = IntArray(n + 1)
+
+        // Time complexity if O(n)
+        for(i in 0 until n + 1) {
+            val firstResult = output[i shr 1]
+            output[i] = firstResult + i % 2
+
         }
         return output
     }
--- src/test/kotlin/com/camilo/leetcode/CoutingbitsTest.kt
+++ src/test/kotlin/com/camilo/leetcode/CoutingbitsTest.kt
@@ -14,8 +14,22 @@ internal class CoutingbitsTest {
 
     @Test
     fun `it should return 0,1,1 a number to the input 2`() {
-        val input = 5
+        val input = 2
         val result = Coutingbits().countBits(input)
+        assertArrayEquals(intArrayOf(0,1,1), result)
+    }
+
+    @Test
+    fun `it should return 0,1,1,2,1,2 a number to the input 5 more efficient`() {
+        val input = 5
+        val result = Coutingbits().countBitsMoreDynamicProgramming(input)
+        assertArrayEquals(intArrayOf(0, 1, 1, 2, 1, 2), result)
+    }
+
+    @Test
+    fun `it should return 0,1,1 a number to the input 2 more efficient`() {
+        val input = 2
+        val result = Coutingbits().countBitsMoreDynamicProgramming(input)
         assertArrayEquals(intArrayOf(0,1,1), result)
     }
 }
