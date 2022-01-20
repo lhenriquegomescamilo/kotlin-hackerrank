b43781aac8d8447f4bcf8ee7bc4457e62ec02439
lcamilo <lhenriquegomescamilo@gmail.com>
2022-01-20 21:46:03 +0000

    Added the memoization with concept of brute force.

--- src/main/kotlin/com/camilo/lessons/dynamic/NumberOfCombination.kt
+++ src/main/kotlin/com/camilo/lessons/dynamic/NumberOfCombination.kt
@@ -1,18 +1,49 @@
 package com.camilo.lessons.dynamic
 
 class NumberOfCombination {
-    fun howSum(targetSum: Int, numbers: List<Int>): ArrayDeque<Int>? {
+    /**
+     * m = targetSum
+     * n = numbers.length
+     *
+     * ## Bruter force
+     * time: O(n^m * m)
+     * space: O(n)
+     */
+    fun howSumBruteForce(targetSum: Int, numbers: List<Int>): ArrayDeque<Int>? {
         if (targetSum == 0) return ArrayDeque()
         if (targetSum < 0) return null
 
         for (num in numbers) {
             val remainder = targetSum - num
-            val result = howSum(remainder, numbers)
+            val result = howSumBruteForce(remainder, numbers)
             if (result != null) {
                 result.addLast(num)
                 return result
             }
         }
         return null
+    }
+
+
+    fun howSumBruteForceMemoization(
+        targetSum: Int,
+        numbers: List<Int>,
+        memoization: MutableMap<Int, ArrayDeque<Int>?> = LinkedHashMap()
+    ): ArrayDeque<Int>? {
+        if (memoization.containsKey(targetSum)) return memoization[targetSum]
+        if (targetSum == 0) return ArrayDeque()
+        if (targetSum < 0) return null
+
+        for (num in numbers) {
+            val remainder = targetSum - num
+            val result = howSumBruteForceMemoization(remainder, numbers, memoization)
+            if (result != null) {
+                result.addLast(num)
+                memoization[targetSum] = result
+                return memoization[targetSum]
+            }
+        }
+        memoization[targetSum] = null
+        return memoization[targetSum]
     }
 }
--- src/test/kotlin/com/camilo/lessons/dynamic/NumberOfCombinationTest.kt
+++ src/test/kotlin/com/camilo/lessons/dynamic/NumberOfCombinationTest.kt
@@ -7,35 +7,78 @@ import org.junit.jupiter.api.Test
 internal class NumberOfCombinationTest {
 
     @Test
-    fun `it should return 4,3 combination of 7 to array of 5, 3, 4, 7`() {
+    fun `it should return 4,3 combination of 7 to array of 5, 3, 4, 7 - BRUTE FORCE`() {
         val input = 7
         val arr = listOf(5, 3, 4, 7)
-        val result = NumberOfCombination().howSum(input, arr)
+        val result = NumberOfCombination().howSumBruteForce(input, arr)
         assertEquals(ArrayDeque(listOf(4, 3)), result)
     }
 
     @Test
-    fun `it should return 3,2,2 combination of 7 to array of 2,3`() {
+    fun `it should return 3,2,2 combination of 7 to array of 2,3 BRUTE FORCE`() {
         val input = 7
         val arr = listOf(2, 3)
-        val result = NumberOfCombination().howSum(input, arr)
+        val result = NumberOfCombination().howSumBruteForce(input, arr)
         assertEquals(ArrayDeque(listOf(3, 2, 2)), result)
     }
 
-
     @Test
-    fun `it should return 2,2,2,2 combination of 8 to array of 2,3`() {
+    fun `it should return 2,2,2,2 combination of 8 to array of 2,3 BRUTE FORCE`() {
         val input = 8
         val arr = listOf(2, 3)
-        val result = NumberOfCombination().howSum(input, arr)
+        val result = NumberOfCombination().howSumBruteForce(input, arr)
         assertEquals(ArrayDeque(listOf(2, 2, 2, 2)), result)
     }
 
     @Test
-    fun `it should return null combination of 7 to array of 2,4`() {
+    fun `it should return null combination of 7 to array of 2,4 BRUTE FORCE`() {
         val input = 7
         val arr = listOf(2, 4)
-        val result = NumberOfCombination().howSum(input, arr)
+        val result = NumberOfCombination().howSumBruteForce(input, arr)
+        assertNull(result)
+    }
+
+
+
+
+
+    @Test
+    fun `it should return 4,3 combination of 7 to array of 5, 3, 4, 7 - BRUTE FORCE Memoization`() {
+        val input = 7
+        val arr = listOf(5, 3, 4, 7)
+        val result = NumberOfCombination().howSumBruteForceMemoization(input, arr)
+        assertEquals(ArrayDeque(listOf(4, 3)), result)
+    }
+
+    @Test
+    fun `it should return 3,2,2 combination of 7 to array of 2,3 BRUTE FORCE Memoization`() {
+        val input = 7
+        val arr = listOf(2, 3)
+        val result = NumberOfCombination().howSumBruteForceMemoization(input, arr)
+        assertEquals(ArrayDeque(listOf(3, 2, 2)), result)
+    }
+
+    @Test
+    fun `it should return 2,2,2,2 combination of 8 to array of 2,3 BRUTE FORCE Memoization`() {
+        val input = 8
+        val arr = listOf(2, 3)
+        val result = NumberOfCombination().howSumBruteForceMemoization(input, arr)
+        assertEquals(ArrayDeque(listOf(2, 2, 2, 2)), result)
+    }
+
+    @Test
+    fun `it should return null combination of 7 to array of 2,4 BRUTE FORCE Memoization`() {
+        val input = 7
+        val arr = listOf(2, 4)
+        val result = NumberOfCombination().howSumBruteForceMemoization(input, arr)
+        assertNull(result)
+    }
+
+    @Test
+    fun `it should return null combination of 300 to array of 7,14 BRUTE FORCE Memoization`() {
+        val input = 300
+        val arr = listOf(7, 14)
+        val result = NumberOfCombination().howSumBruteForceMemoization(input, arr)
         assertNull(result)
     }
 
