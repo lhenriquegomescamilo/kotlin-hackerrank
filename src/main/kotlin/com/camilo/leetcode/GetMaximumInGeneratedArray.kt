b181e9a00b76d3238dfcace4b6885331362e0079
lcamilo <lhenriquegomescamilo@gmail.com>
2022-01-27 21:39:10 +0000

    Added the complexity of methods.

--- src/main/kotlin/com/camilo/leetcode/GetMaximumInGeneratedArray.kt
+++ src/main/kotlin/com/camilo/leetcode/GetMaximumInGeneratedArray.kt
@@ -1,6 +1,10 @@
 package com.camilo.leetcode
 
 class GetMaximumInGeneratedArray {
+    /**
+     * Time: O(n)
+     * Space: O(n)
+     */
     fun getMaximumGenerated(n: Int): Int {
         if(n == 2 || n == 1) return 1
         if(n ==  0) return 0
