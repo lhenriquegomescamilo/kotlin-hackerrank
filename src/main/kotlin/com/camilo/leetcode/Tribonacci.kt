006ce55ee98373784a389ad39abf1979643e74f7
lcamilo <lhenriquegomescamilo@gmail.com>
2022-01-27 19:59:37 +0000

    Solved the tribonacci.

--- src/main/kotlin/com/camilo/leetcode/Tribonacci.kt
+++ src/main/kotlin/com/camilo/leetcode/Tribonacci.kt
@@ -2,6 +2,18 @@ package com.camilo.leetcode
 
 class Tribonacci {
     fun tribonacci(n: Int): Int {
-        return 0
+        if(n == 1 || n == 2 ) return 1
+        var t0 = 0
+        var t1 = 1
+        var t2 = 1
+        var tn = 0
+        for(i in 2 until n) {
+            tn = t0 + t1 + t2
+            t0 = t1
+            t1 = t2
+            t2 = tn
+        }
+        return tn
     }
+
 }
