c1ba3aa402032c6b8fff87260eb4b41a187f0567
Luis Camilo (CTW) <Luis.Camilo@ctw.bmwgroup.com>
2022-07-03 21:36:44 +0100

    feat(FindNumbersEvenNumberDigits): using pointer to navigate by right and left directing using an new array

--- src/main/kotlin/com/camilo/leetcode/SquareOfSortedArray.kt
+++ src/main/kotlin/com/camilo/leetcode/SquareOfSortedArray.kt
@@ -1,5 +1,7 @@
 package com.camilo.leetcode
 
+import kotlin.math.abs
+
 /**
  * https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3240/
  *
@@ -10,6 +12,9 @@ class SquareOfSortedArray {
      * Output: [0,1,9,16,100]
      * Explanation: After squaring, the array becomes [16,1,0,9,100].
      * After sorting, it becomes [0,1,9,16,100].
+     *
+     * time complexity is: O(m + n^2)
+     * space complexity is: O(3 + n) -> O(n)
      */
     fun sortedSquare(nums: IntArray): IntArray {
         var temp: Int
@@ -19,9 +24,9 @@ class SquareOfSortedArray {
             nums[index] = currentPower;
         }
 
-        for(index in nums.indices) {
-            for(j in (index + 1) until nums.size) {
-                if(nums[index] > nums[j]) {
+        for (index in nums.indices) {
+            for (j in (index + 1) until nums.size) {
+                if (nums[index] > nums[j]) {
                     temp = nums[index]
                     nums[index] = nums[j]
                     nums[j] = temp
@@ -30,4 +35,33 @@ class SquareOfSortedArray {
         }
         return nums
     }
+
+    /**
+     * Input: nums = [-4,-1,0,3,10]
+     * Output: [0,1,9,16,100]
+     * Explanation: After squaring, the array becomes [16,1,0,9,100].
+     * After sorting, it becomes [0,1,9,16,100].
+     *
+     * time complexity is: O(n)
+     * space complexity is: O(m + n)
+     */
+    fun sortedSquareLinear(nums: IntArray): IntArray {
+        var leftIndex = 0
+        var rightIndex = nums.lastIndex
+        var pointer = nums.lastIndex
+        val output = IntArray(nums.size)
+
+        while (leftIndex <= rightIndex) {
+            if (abs(nums[leftIndex]) > abs(nums[rightIndex])) {
+                output[pointer] = nums[leftIndex] * nums[leftIndex]
+                leftIndex++
+            } else {
+                output[pointer] = nums[rightIndex] * nums[rightIndex]
+                rightIndex--
+            }
+            pointer--
+        }
+        return output
+    }
+
 }
--- src/test/kotlin/com/camilo/leetcode/SquareOfSortedArrayTest.kt
+++ src/test/kotlin/com/camilo/leetcode/SquareOfSortedArrayTest.kt
@@ -13,4 +13,12 @@ internal class SquareOfSortedArrayTest {
         println(sortedSquare.contentToString())
         Assertions.assertArrayEquals(intArrayOf(0, 1, 9, 16, 100), sortedSquare)
     }
+
+    @Test
+    fun `should return array of 0,1,9,16,100 to input array of -4,-1,0,3,10 linear algorithm`() {
+        val inputArray = intArrayOf(-4, -1, 0, 3, 10)
+        val sortedSquare = SquareOfSortedArray().sortedSquareLinear(inputArray)
+        println(sortedSquare.contentToString())
+        Assertions.assertArrayEquals(intArrayOf(0, 1, 9, 16, 100), sortedSquare)
+    }
 }
