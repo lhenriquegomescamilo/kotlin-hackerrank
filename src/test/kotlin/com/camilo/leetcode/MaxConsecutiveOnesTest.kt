32d9788bca54db1e8c8bb975be8c3fccc073630b
lcamilo <lhenriquegomescamilo@gmail.com>
2022-07-04 13:14:58 +0100

    feat(MaxConsecutiveOnes): finding the max consecutive algorithm

--- src/test/kotlin/com/camilo/leetcode/MaxConsecutiveOnesTest.kt
+++ src/test/kotlin/com/camilo/leetcode/MaxConsecutiveOnesTest.kt
@@ -1,6 +1,5 @@
 package com.camilo.leetcode
 
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
@@ -9,13 +8,13 @@ internal class MaxConsecutiveOnesTest {
     @Test
     fun `should return 3 when receive the array of 1,1,0,1,1,1`() {
         val input = intArrayOf(1, 1, 0, 1, 1, 1)
-        val ouptut = MaxConsecutiveOnes().findMaxConsecutiveOnes(input)
-        assertEquals(3, ouptut)
+        val output = MaxConsecutiveOnes().findMaxConsecutiveOnes(input)
+        assertEquals(3, output)
     }
     @Test
     fun `should return 2 when receive the array of 1,0,1,1,0,1`() {
         val input = intArrayOf(1,0,1,1,0,1)
-        val ouptut = MaxConsecutiveOnes().findMaxConsecutiveOnes(input)
-        assertEquals(2, ouptut)
+        val output = MaxConsecutiveOnes().findMaxConsecutiveOnes(input)
+        assertEquals(2, output)
     }
 }
