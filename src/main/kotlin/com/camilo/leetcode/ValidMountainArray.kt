972246881d75f23d797b654a8c01c3363baad01f
lcamilo <lhenriquegomescamilo@gmail.com>
2022-07-27 23:44:17 +0100

    feat(CheckIfDoubleExist): trying to define the complexity from algorithm

--- src/main/kotlin/com/camilo/leetcode/ValidMountainArray.kt
+++ src/main/kotlin/com/camilo/leetcode/ValidMountainArray.kt
@@ -4,21 +4,22 @@ package com.camilo.leetcode
  * https://leetcode.com/explore/learn/card/fun-with-arrays/527/searching-for-items-in-an-array/3251/
  */
 class ValidMountainArray {
-
+    /**
+     * Complexity time: O(n)
+     * space complexity: O(1)
+     */
     fun validMountainArray(arr: IntArray): Boolean {
         if (arr.size <= 2) return false
 
-        var leftIndex = 0
         var timesDown = 0
         var isUp = false
-        for (i in 1..arr.lastIndex) {
+        for ((leftIndex, i) in (1..arr.lastIndex).withIndex()) {
             isUp = arr[leftIndex] < arr[i]
             if (!isUp) {
                 timesDown++
             }
 
             if ((timesDown > 0 && isUp ) || arr[leftIndex] == arr[i]) return false
-            leftIndex++
         }
 
         return !isUp && timesDown > 0 && timesDown != (arr.size - 1)
